03e1e72b055bfe0c046b7b36033b761cdc204c86
Eric Rynerson <eric.rynerson@gmail.com>
2017-08-26 04:58:12 -0500

    update setup

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
 from distutils.core import setup
 setup(
   name = 'session2s3',
-  packages = ['session2s3'], # this must be the same as the name above
-  version = '0.1',
-  description = 'A random test lib',
+  packages = ['session2s3'],
+  version = '0.1a1',
+  description = 'Save your Python session to S3',
   author = 'Eric Rynerson',
   author_email = 'eric.rynerson@gmail.com',
+  license='MIT',
   url = 'https://github.com/ryninho/session2s3',
   download_url = 'https://github.com/ryninho/session2s3/archive/0.1.tar.gz',
   keywords = ['s3', 'logging', 'debugging', 'session', 'rollback'],
-  classifiers = [],
+  classifiers = [
+    'Development Status :: 3 - Alpha',
+    'Programming Language :: Python :: 2.7'
+  ],
+  install_requires=['boto3', 'dill'],
+  python_requires='==2.7.*',
 )
