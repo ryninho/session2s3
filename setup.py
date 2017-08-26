c06a8857095539a607097d53ce99df812c0d45ae
Eric Rynerson <eric.rynerson@gmail.com>
2017-08-26 04:30:12 -0500

    add setup files and tag

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'session2s3',
+  packages = ['session2s3'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A random test lib',
+  author = 'Eric Rynerson',
+  author_email = 'eric.rynerson@gmail.com',
+  url = 'https://github.com/ryninho/session2s3',
+  download_url = ''
+  keywords = ['s3', 'logging', 'debugging', 'session', 'rollback'],
+  classifiers = [],
+)
