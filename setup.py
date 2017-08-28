17e7da76fe7032eb6dc0cffd4d533d0d1de3afa6
Eric Rynerson <eric.rynerson@gmail.com>
2017-08-27 20:01:38 -0700

    use new version number

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 setup(
   name = 'session2s3',
   packages = ['session2s3'],
-  version = '0.1a1',
+  version = '0.2a1',
   description = 'Save your Python session to S3',
   author = 'Eric Rynerson',
   author_email = 'eric.rynerson@gmail.com',
   license='MIT',
   url = 'https://github.com/ryninho/session2s3',
-  download_url = 'https://github.com/ryninho/session2s3/archive/0.1.tar.gz',
+  download_url = 'https://github.com/ryninho/session2s3/archive/0.2.tar.gz',
   keywords = ['s3', 'logging', 'debugging', 'session', 'rollback'],
   classifiers = [
     'Development Status :: 3 - Alpha',
